fe3a0a5836a2935ad7c510412650923b0e891039
Patrick Marques <patrickmarques2009@hotmail.com>
2024-09-23 16:28:12 -0300

    feat: :sparkles: Adicionadas as rotas de CRUD da Tabela de Veiculos

---
+++ minimal-api-cadastro-veiculos/Dominio/Servicos/VeiculoServico.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using minimal_api_cadastro_veiculos.Dominio.Entidades;
+using minimal_api_cadastro_veiculos.Dominio.Interfaces;
+using minimal_api_cadastro_veiculos.Infraestrura.Db;
+
+namespace minimal_api_cadastro_veiculos.Dominio.Servicos;
+
+
+public class VeiculoServico : IVeiculoServico
+{
+    private readonly DbContexto _contexto;
+    public VeiculoServico(DbContexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    public void Incluir(Veiculo veiculo)
+    {
+        _contexto.Veiculos.Add(veiculo);
+        _contexto.SaveChanges();
+    }
+
+    public List<Veiculo> Todos(int pagina, string nome, string marca)
+    {
+        var query = _contexto.Veiculos.AsQueryable();
+        if(!string.IsNullOrEmpty(nome))
+        {
+            query = query.Where(v => EF.Functions.Like(v.Nome.ToLower(), $"%{nome}%"));
+        }
+
+        int itensPorPagina = 10;
+
+        query = query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
+        
+        return query.ToList();
+    }
+
+    public Veiculo? BuscaPorId(int id)
+    {
+        return _contexto.Veiculos.Where(v => v.id == id).FirstOrDefault();
+    }
+
+    public void Atualizar(Veiculo veiculo)
+    {
+        _contexto.Veiculos.Update(veiculo);
+        _contexto.SaveChanges();
+    }
+
+    public void Excluir(Veiculo veiculo)
+    {
+        _contexto.Veiculos.Remove(veiculo);
+        _contexto.SaveChanges();
+    }
+}
