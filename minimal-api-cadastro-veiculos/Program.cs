860e965f6eae4e10018a1f3e3ce3f0a5916337ee
Patrick Marques <patrickmarques2009@hotmail.com>
2024-09-24 14:45:28 -0300

    docs: :memo: Adicionadas tags para organização das rotas na documentação do Swagger

--- minimal-api-cadastro-veiculos/Program.cs
+++ minimal-api-cadastro-veiculos/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddDbContext<DbContexto>(options => {
 var app = builder.Build();
 
 #region Home
-app.MapGet("/", () => Results.Json(new Home()));
+app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
 #endregion
 
 #region Administradores
@@ -37,7 +37,7 @@ app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministra
         return Results.Ok("Login realizado com sucesso!");
     else
         return Results.Unauthorized();
-});
+}).WithTags("Administradores");
 #endregion
 
 #region Veiculos
@@ -52,14 +52,14 @@ app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veic
     veiculoServico.Incluir(veiculo);
 
     return Results.Created($"/veiculo/{veiculo.id}", veiculo);
-});
+}).WithTags("Veiculos");
 
 app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) => {
     
     var veiculos = veiculoServico.Todos(pagina);
 
     return Results.Ok(veiculos);
-});
+}).WithTags("Veiculos");
 #endregion
 
 app.UseSwagger();
