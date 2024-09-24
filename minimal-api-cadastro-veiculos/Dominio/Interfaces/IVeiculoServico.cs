937506eeeff6765be6b4f010cdaaac795e4f0529
Patrick Marques <patrickmarques2009@hotmail.com>
2024-09-24 14:37:02 -0300

    feat: :sparkles: Adicionada rota de Get para consulta de veículos cadastrados por página

--- minimal-api-cadastro-veiculos/Dominio/Interfaces/IVeiculoServico.cs
+++ minimal-api-cadastro-veiculos/Dominio/Interfaces/IVeiculoServico.cs
@@ -1,14 +1,12 @@
-using minimal_api_cadastro_veiculos.Dominio.DTOs;
 using minimal_api_cadastro_veiculos.Dominio.Entidades;
 
 namespace minimal_api_cadastro_veiculos.Dominio.Interfaces;
 
 public interface IVeiculoServico
 {
-    List<Veiculo> Todos(int pagina, string? nome, string? marca);
+    List<Veiculo> Todos(int? pagina = 1, string? nome = null, string? marca = null);
     Veiculo? BuscaPorId(int id);
     void Incluir(Veiculo veiculo);
     void Atualizar(Veiculo veiculo);
     void Excluir(Veiculo veiculo);
-
 }
